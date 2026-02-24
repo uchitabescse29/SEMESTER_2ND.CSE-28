cf9310dd4d85d7143faedf441950c46fa7e282fa
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-02-24 15:18:10 +0530

    1

---
+++ script.js
@@ -0,0 +1,79 @@
+// console.log("INTRODUCTION TO JS");
+// const x=100;
+// console.log(x);
+
+//  let a=10;
+// console.log(a);
+//  a=30;
+// var a;
+// console.log(a);
+// var a=10;
+// var b=20;
+// var c=a+b;
+// console.log(c);
+// let a="string";
+// console.log(a);
+// let b=1234;
+// console.log(b);
+// let c=true/false;
+// console.log(c);
+// const { log } = require("console");
+
+/*console.log("hello world");
+/*var a=10 ;
+var a=20;
+console.log(a);*/
+/* var b = 20;
+var c = a + b;
+console.log(c);*/
+//console.log("let introduction");
+//let a=10;
+////a=90;
+//let a=30;
+///console.log(a);
+//const x=100;
+//x=500;
+//console.log(x);
+//data tupes(primitive{7types} and non primitive{object})
+/*let name = "Alice";        // String
+let age = 30;              // Number
+let isStudent = true;      // Boolean
+let score;                 // Undefined
+let car = null;            // Null
+let person = { name: "Bob", age: 25 }; // Object
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(score);
+console.log(car);
+console.log(person);*/
+
+//operators in JS(•	Arithmetic Operators)
+// let a=10;
+// let b=20;
+// console.log(a+b);
+// console.log(a-b);   
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+//•	Assignment Operators: =, +=, -=, *=, /=, %=
+// let c=10;
+// c+=5;
+// console.log(c);
+// c-=3;
+// console.log(c);
+// c*=2;
+// console.log(c);
+// c/=4;
+// console.log(c);
+// c%=3;
+// console.log(c);
+
+//•	Comparison Operators: == (loose equality), === (strict equality), !=, !==, >, <, >=, <=.
+// let x=10;
+// let y="10";
+// console.log(x==y);
+// console.log(x===y);
+// console.log(x!=y);
+// console.log(x!==y);
