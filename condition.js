7156463a725c6c7ed6d9a1862462c9581aeb9a86
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-02-24 15:45:34 +0530

    codition.js

---
+++ condition.js
@@ -0,0 +1,44 @@
+// let age = 3;
+// if (age < 18) {
+//     console.log("You are a minor.");
+// }else if (age >= 18 && age < 65) {
+//     console.log("You are an adult.");
+// }else {
+//     console.log("You are a senior.");
+// }
+
+
+// let day = "Monday";
+// if (day === "Monday") {
+//     console.log("Today is Monday.");
+// } else if (day === "Tuesday") {
+//     console.log("Today is Tuesday.");
+// }else {
+//     console.log("Today is not Monday or Tuesday.");
+// }
+// let day = 6;
+// switch (day) {
+//     case 1:
+//         console.log("Today is Monday.");
+//         break;
+//     case 2:
+//         console.log("Today is Tuesday.");
+//         break;
+//     case 3:
+//         console.log("Today is Wednesday.");
+//         break;
+//     case 4:
+//         console.log("Today is Thursday.");
+//         break;
+//     case 5:
+//         console.log("Today is Friday.");
+//         break;
+//     case 6:
+//         console.log("Today is Saturday.");
+//         break;
+//     case 7:
+//         console.log("Today is Sunday.");
+//         break;
+//     default:
+//         console.log("Invalid day number.");
+// }
