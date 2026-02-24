67c186eb903ef6181fb0698cbf009664aa8cd543
uchitabescse29 <uchit.25b01010827@abes.ac.in>
2026-02-24 22:52:52 +0530

    loops.js

---
+++ loops.js
@@ -0,0 +1,13 @@
+let a = 5;
+for (let i = 0; i < 5; i++) {
+    console.log("The value of a is: " + a);
+    a++;
+}
+while (a <= 10) {
+    console.log("The value of a is: " + a);
+    a++;
+}
+do {
+    console.log("The value of a is: " + a);
+    a++;
+} while (a < 10);
